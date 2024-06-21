4b1ee2632291bd3e4cf5f02f9cd8fd2ab484cb1b
wyy <969021416@qq.com>
2024-06-21 08:30:50 +0800

    first commit

---
+++ app/src/main/java/com/wyy/example/MainActivity.java
@@ -0,0 +1,43 @@
+package com.wyy.example;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.dialog.MaterialDialogs;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+//        new MaterialAlertDialogBuilder(this, com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered)
+//                .setTitle("标题")
+//                .setMessage("消息")
+//                .setNeutralButton("取消", (dialog, which) -> {
+//
+//                })
+//                .setNegativeButton("拒绝", (dialog, which) -> {
+//
+//                })
+//                .setPositiveButton("同意", (dialog, which) -> {
+//
+//                })
+//                .show();
+    }
+}
---
+++ mylibrary/src/main/java/com/wyy/mylibrary/VersionUtils.java
@@ -0,0 +1,14 @@
+package com.wyy.mylibrary;
+
+/**
+ * @author: 王勇勇
+ * @date: 2024/6/20
+ * @description：
+ */
+public class VersionUtils {
+
+    public static String getVersions() {
+        return "1.0.0";
+    }
+
+}
